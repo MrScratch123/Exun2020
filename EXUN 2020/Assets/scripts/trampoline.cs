75c6efa384797d312e656bad89ecdae1918a4f24
MrScratch <mithalaarush@gmail.com>
2020-11-17 16:44:07 +0530

    Update trampoline.cs

--- EXUN 2020/Assets/scripts/trampoline.cs
+++ EXUN 2020/Assets/scripts/trampoline.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class trampoline : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         
     }
